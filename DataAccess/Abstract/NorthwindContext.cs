0e388b3e77e55fac81dfd4f5fd2d8ca98cf0e114
ofarukcelik <ofarukcelik@outlook.com>
2021-02-21 17:40:18 +0300

    Entity framework dal used

--- DataAccess/Abstract/NorthwindContext.cs
+++ DataAccess/Abstract/NorthwindContext.cs
@@ -13,5 +13,18 @@ namespace DataAccess.Abstract
             optionsBuilder.UseSqlServer(@"Server=DESKTOP-3EVB481;Database=Northwind;Trusted_Connection=true");
         }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Fluent Mapping
+            //modelBuilder.HasDefaultSchema("admin");
+            modelBuilder.Entity<Employee>().ToTable("Employees");
+
+            modelBuilder.Entity<Employee>().Property(p => p.ID).HasColumnName("EmployeeID");
+            modelBuilder.Entity<Employee>().Property(p => p.FirstName).HasColumnName("FirstName");
+            modelBuilder.Entity<Employee>().Property(p => p.LastName).HasColumnName("LastName");
+        }
     }
 }
---
+++ DataAccess/Concrete/EfEmployeeDal.cs
@@ -0,0 +1,41 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class EfEmployeeDal : IEmployeeDal
+    {
+        public void Add(Employee entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Employee entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Get(Expression<Func<Employee, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetAll(Expression<Func<Employee, bool>> filter = null)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return filter == null ? context.Set<Employee>().ToList() : context.Set<Employee>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Employee entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
