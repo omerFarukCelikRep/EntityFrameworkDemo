b5c778ec68022cb212d940fe59491a496a7b4e28
ofarukcelik <ofarukcelik@outlook.com>
2021-02-21 15:11:54 +0300

    northwind data simulated

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Linq;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetAll();
+
+            GetProductsByCategoryId(2);
+        }
+
+        private static void GetAll()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                foreach (Product product in context.Products)
+                {
+                    Console.WriteLine(product.ProductName);
+                }
+            }
+        }
+        private static void GetProductsByCategoryId(int categoryId)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = context.Products.Where(p => p.CategoryID == categoryId);
+
+                foreach (Product product in result)
+                {
+                    Console.WriteLine(product.ProductName);
+                }
+            }
+        }
+    }
+}
