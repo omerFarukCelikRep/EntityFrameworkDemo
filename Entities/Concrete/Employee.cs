14ad9e6992f2e74f3f2f180aeb52ee0c3804e04b
ofarukcelik <ofarukcelik@outlook.com>
2021-02-21 17:39:36 +0300

    Employee added

---
+++ Entities/Concrete/Employee.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Employee : IEntity
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
